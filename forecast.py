d21b2ecb3b6451a1f84a7684141124061aaaa287
schmodd <schmodd@enexo.de>
2015-03-20 12:28:11 +0100

    using prettytable now

--- forecast.py
+++ forecast.py
@@ -8,6 +8,7 @@ import json
 import datetime
 import argparse
 import sys
+from prettytable import PrettyTable
 
 # surf to https://developer.forecast.io/ for an api key
 # use http://dbsgeo.com/latlon/ to get coordinates for your location
@@ -16,30 +17,40 @@ LAT=''
 LONG=''
 #some api settings
 UNITS='si' # auto possibly shows wrong measuring unit
-LANG='de'
+LANG='en'
 
-def formatDatetime(unixTime, outputFormat='%d %b %H:%M'):
+def formatDatetime(unixTime, outputFormat='%d. %b. %H:%M'):
     return datetime.datetime.fromtimestamp(unixTime).strftime(outputFormat)
 
 def getMeasuringUnit():
     return '°F' if UNITS == 'us' else '°C'
 
-def showDaily(limit):
-    print('\n{0:6}    {1:}    {2:}    {3:}'.format('Date', 'Min ' + getMeasuringUnit(), 'Max ' + 
-        getMeasuringUnit(), 'Summary'))
+def showDaily(limit, measuring_unit):
+    HEAD = ['Date', 'Temp min', 'Temp max', 'Humidity', 'Summary']
+    table = PrettyTable(HEAD, border = False, padding_width = 2)
+    table.align='r'
+    table.align['Date'] = 'l'
+    table.align['Summary'] = 'l'
     for day in result['daily']['data'][0:limit]:
-        print('{0:6}    {1:>6.2f}    {2:>6.2f}    {3:50}'.format(formatDatetime(day['time'], '%d %b'), 
-            day['temperatureMin'], day['temperatureMax'], day['summary']))
+        table.add_row([formatDatetime(day['time'], '%d. %b.'), '{:6.2f} {:2}'.format(day['temperatureMin'], 
+            measuring_unit), '{:6.2f} {:2}'.format(day['temperatureMax'], measuring_unit), 
+            '{:2.0f} {:1}'.format(day['humidity']*100, '%'), day['summary']]) 
+    print('\n', end='')
+    print(table)
 
-def showHourly(limit):
-    print('\n{0:6}    {1:}    {2:}    {3:}'.format('Date', 'Time', 'Temp ' + getMeasuringUnit(), 'Summary'))
+def showHourly(limit, measuring_unit):
+    HEAD = ['DateTime', 'Temp', 'Humidity', 'Summary']
+    table = PrettyTable(HEAD, border = False, padding_width = 2)
+    table.align='r'
+    table.align['DateTime'] = 'l'
+    table.align['Summary'] = 'l'
     for hour in result['hourly']['data'][0:limit]:
-        print('{0:6}    {1:5}    {2:>6.2f}    {3:50}'.format(formatDatetime(hour['time'], '%d %b'),
-            formatDatetime(hour['time'], '%H:%M'), hour['temperature'], hour['summary']))
+        table.add_row([formatDatetime(hour['time'], '%d. %b. %H:%M'), '{:6.2f} {:2}'.format(hour['temperature'], 
+            measuring_unit), '{:2.0f} {:1}'.format(hour['humidity']*100, '%'), hour['summary']])
+    print('\n', end='')
+    print(table)
 
 if __name__ == '__main__':
-
-    # setup args
     parser = argparse.ArgumentParser(description='weather forecast powered by forecast.io')
     parser.add_argument('-d', help='daily forecast', action='store_true')
     parser.add_argument('-ho', help='hourly forecast', action='store_true')
@@ -52,18 +63,23 @@ if __name__ == '__main__':
     URL = BASE_URL + SETTINGS
     HTTP_HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:33.0) Gecko/20100101 Firefox/33.0',
         'Accept-Encoding': 'gzip'}
+    MEAS_UNIT = getMeasuringUnit()
 
     if args.d:
         URL += 'hourly,currently'
-        result = requests.get(URL, headers=HTTP_HEADERS).json()
-        showDaily(args.l)
     elif args.ho:
         URL += 'daily,currently'
-        result = requests.get(URL, headers=HTTP_HEADERS).json()
-        showHourly(args.l)
     else:
         URL += 'hourly,daily'
-        result = requests.get(URL, headers=HTTP_HEADERS).json()
-        print('\ntime: '+ formatDatetime(result['currently']['time']) + ' | temp: ' + 
-            str(result['currently']['temperature']) + ' ' + getMeasuringUnit() + ' | humidity: ' + 
-            str(result['currently']['humidity']*100) + ' %')
+
+    result = requests.get(URL, headers=HTTP_HEADERS).json()
+
+    if args.d:
+        print(URL)
+        showDaily(args.l, MEAS_UNIT)
+    elif args.ho:
+        showHourly(args.l, MEAS_UNIT)
+    else:
+        print('{:} {:10}'.format('\n time:', formatDatetime(result['currently']['time'])), end='')
+        print('{:} {:6.2f} {:2}'.format(' | temp:', result['currently']['temperature'], MEAS_UNIT), end='')
+        print('{:} {:2.0f} {:1}'.format(' | humidity:', result['currently']['humidity']*100, '%'))
